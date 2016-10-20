e280b3023a2bfe7e0835381fca7175867e21e73b
Google1234 <number1jtao@gmail.com>
2016-10-21 00:49:18 +0800

    sort

--- Config.py
+++ Config.py
@@ -1,26 +1,29 @@
 class file_offline_train_line():
     def __init__(self):
-        self.User_id=0
-        self.Merchant_id=1
-        self.Coupon_id=2
-        self.Discount_rate=3
-        self.Distance=4
-        self.Date_received=5
-        self.Date=6
+        self.dic={}
+        self.dic["User_id"] = 0
+        self.dic["Merchant_id"] = 1
+        self.dic["Coupon_id"] = 2
+        self.dic["Discount_rate"] = 3
+        self.dic["Distance"] = 4
+        self.dic["Date_received"] = 5
+        self.dic["Date"] = 6
 class file_online_train_line():
     def __init__(self):
-        self.User_id=0
-        self.Merchant_id=1
-        self.Action=2
-        self.Coupon_id=3
-        self.Discount_rate=4
-        self.Date_received=5
-        self.Date=6
+        self.dic={}
+        self.dic["User_id"] = 0
+        self.dic["Merchant_id"] = 1
+        self.dic["Action"] = 2
+        self.dic["Coupon_id"] = 3
+        self.dic["Discount_rate"] = 4
+        self.dic["Date_received"] = 5
+        self.dic["Date"] = 6
 class file_offline_test_line():
     def __init__(self):
-        self.User_id=0
-        self.Merchant_id=1
-        self.Coupon_id=2
-        self.Discount_rate=3
-        self.Distance=4
-        self.Date_received=5
+        self.dic={}
+        self.dic["User_id"] = 0
+        self.dic["Merchant_id"] = 1
+        self.dic["Coupon_id"] = 2
+        self.dic["Discount_rate"] = 3
+        self.dic["Distance"] = 4
+        self.dic["Date_received"] = 5
---
+++ tools.py
@@ -0,0 +1,30 @@
+'''
+#sort by User_id or else
+import pandas
+import Config
+path="Data/"
+
+file="ccf_offline_stage1_train.csv"
+file_format=Config.file_offline_train_line()
+rats = pandas.read_csv(path+file,header=None)
+for key in file_format.dic :
+    rats.sort_values(by=file_format.dic[key],inplace=True)#user_id
+    rats.to_csv(path+file[:-3]+"_sorted_by_"+key+".csv",index=None,header=None)
+del rats
+
+file="ccf_online_stage1_train.csv"
+file_format=Config.file_online_train_line()
+rats = pandas.read_csv(path+file,header=None)
+for key in file_format.dic :
+    rats.sort_values(by=file_format.dic[key],inplace=True)#user_id
+    rats.to_csv(path+file[:-3]+"_sorted_by_"+key+".csv",index=None,header=None)
+del rats
+
+file="ccf_offline_stage1_test_revised.csv"
+file_format=Config.file_offline_test_line()
+rats = pandas.read_csv(path+file,header=None)
+for key in file_format.dic :
+    rats.sort_values(by=file_format.dic[key],inplace=True)#user_id
+    rats.to_csv(path+file[:-3]+"_sorted_by_"+key+".csv",index=None,header=None)
+del rats
+'''
